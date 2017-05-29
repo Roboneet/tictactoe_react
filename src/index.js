f6e8d4075f81283825913c9af4376dacc870e5c8
Roboneet <neethumariyajoy@gmail.com>
2017-05-30 02:47:33 +0530

    removed some hard coding

--- src/index.js
+++ src/index.js
@@ -44,25 +44,26 @@ class Board extends React.Component {
   }
 
   render() {
+    let board = [[0,1,2],[3,4,5],[6,7,8]]
     
+    let my_dom = board.map((row,i) => {
+      
+      var k = row.map((ele,j)=>{
+        return this.renderSquare(ele);
+      },this);
+      return (<div className="board-row">
+          {k}
+        </div>
+      );
+
+      
+    },this);
+
     return (
       <div>
         
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  
+        {my_dom}
       </div>
     );
   }
